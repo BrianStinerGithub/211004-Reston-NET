7b96491cad0ed72ee5ac39a801347ca168ffc926
Brian Stiner <Brian.Stiner@revature.net>
2021-10-05 17:05:38 -0400

    Read in name and say hello!

---
+++ BrianStiner-C-Hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BrianStiner_C_Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            
+            Console.WriteLine("Give me your name.");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello there, "+ name + "!");
+
+        }
+    }
+}
